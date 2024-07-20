f28271a246f5869fa78c52359bc582d6aec8893f
Nitin Choudhary <choudharynitin888@gmail.com>
2024-07-20 14:55:46 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Nitin';
+
+export const experiences = [
+    {
+        'company': 'Infineon Technologies',
+        'role': 'Industrial Automation Specialist',
+        'duration': 'July 2024 - Present',
+        'points': ['What i did in internship to write here...'],
+        // 'url': 'https://inncircles.com/#home',
+    },
+    {
+        'company': 'company name',
+        'role': 'role',
+        'duration': 'duration',
+        'points': ['What i did in internship to write here...'],
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
